bfb2a5e6dd663d6e4ed1280240924316d0b4a25f
Juhana Laakso <juhana.laakso@aalto.fi>
2024-12-19 13:40:28 +0200

    Initialize backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: '../backend/tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+    },
+  ],
+};
