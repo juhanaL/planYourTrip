9cd21120e4ca17135f256bf3f95f9fef78dfa5ed
Juhana Laakso <juhana.laakso@aalto.fi>
2024-12-28 13:17:35 +0200

    Fix typo on HomePage and build project for deployment

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     'Need inspiration for choosing your next destination? Use Weather map and click anywhere around the world to check out the current weather and a webcam picture from your dream destination!';
 
   const todoInfoText =
-    'Planning a trip can get complicated... Use Trip To Do to keep on track with all your necessary tasks before your vaction can begin!';
+    'Planning a trip can get complicated... Use Trip To Do to keep on track with all your necessary tasks before your vacation can begin!';
   return (
     <div className="home-page-container">
       <h1 className="home-page-header">Plan your next trip with Plan Your Trip!</h1>
